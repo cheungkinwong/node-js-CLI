ff8e8f0da14565998829daad43c9d066b79d5145
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-14 12:10:50 +0200

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // javascript
-console.log("Hello, Node.JS!");
+const axios = require("axios");
+// const translateThis = "process.translateThis";
+const from = "en";
+const to = "chi";
+
+const standard_input = process.stdin;
+standard_input.setEncoding("utf-8");
+
+console.log("Please input text you want to translate.");
+
+standard_input.on("data", function(data) {
+     axios({
+          method: "get",
+          url: `https://api.mymemory.translated.net/get?q=${data}&langpair=${from}|${to}`,
+          responseType: "json"
+     }).then(function(response) {
+          console.log(response.data.responseData.translatedText);
+     });
+});
