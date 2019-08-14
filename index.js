69abab1fe7f73b0e1f2a4e63ec42bec8be2cbd3a
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-14 16:53:56 +0200

    simplified code

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // javascript
 const axios = require("axios");
-// const translateThis = "process.translateThis";
-const from = "en";
-const to = "chi";
-
+const from = "";
 const standard_input = process.stdin;
 standard_input.setEncoding("utf-8");
 
-console.log("Please input text you want to translate.");
-
-standard_input.on("data", function(data) {
-     axios({
-          method: "get",
-          url: `https://api.mymemory.translated.net/get?q=${data}&langpair=${from}|${to}`,
-          responseType: "json"
-     }).then(function(response) {
-          console.log(response.data.responseData.translatedText);
+console.log("Please input language you want to translate to.");
+standard_input.on("data", function translate(input_language) {
+     console.log("Please input text you want to translate.");
+     standard_input.on("data", function(data) {
+          axios({
+               method: "get",
+               url: `https://api.mymemory.translated.net/get?q=${data}&langpair=${from}|${input_language}`,
+               responseType: "json"
+          }).then(function(response) {
+               console.log(response.data.responseData.translatedText);
+          });
      });
 });
